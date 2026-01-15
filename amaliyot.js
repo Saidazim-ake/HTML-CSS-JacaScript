fc9d31bb49648a4fd2c9b5005899d06ec142fe25
Saidazim Shamshiyev <macbookpro13dyum@gmail.com>
2026-01-15 16:47:59 +0500

    Bu yerda sodda algoritm
    ..

--- amaliyot.js
+++ amaliyot.js
@@ -1,6 +1,33 @@
-let i = 1;
+ let d = 1;
 
-while (i <= 10) {
-  console.log(i);
-  i++;
-}
+ while (d <= 10) {
+   console.log(d);
+   d++;
+ }
+
+ let is = 10;
+
+ while (ii >= 1) {
+   console.log(ii);
+   ii--;
+ }
+
+ let i = 0;
+
+ while (i <= 20) {
+   if (i % 2 === 0) {
+     console.log(i);
+   }
+   i++;
+ }
+
+  let s = 5;
+
+  do {
+    console.log(s);
+    s--;
+  } while (s >= 1);
+
+
+
+
