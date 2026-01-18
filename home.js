2927594ab9bc4ba94f6f964c31ff57a8358e40d6
Saidazim Shamshiyev <macbookpro13dyum@gmail.com>
2026-01-18 08:24:21 +0500

    JUFT va TOQ

---
+++ home.js
@@ -0,0 +1,10 @@
+function juftmiToqmi(son) {
+    if (son % 2 === 0) {
+        return "Juft";
+    } else {
+        return "Toq";
+    }
+}
+console.log(juftmiToqmi(2));
+console.log(juftmiToqmi(1));
+console.log(juftmiToqmi(5));
