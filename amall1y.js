86ff5c91998e4ef2d135b2278a80d468e934ffe9
Saidazim Shamshiyev <macbookpro13dyum@gmail.com>
2026-01-24 15:23:01 +0500

    hech nima yo'q

--- amall1y.js
+++ amall1y.js
@@ -1,17 +1,1 @@
-let mevami = ["olma", "banan", "apelsin", "shaftoli"];
-mevami.forEach((meva, index) => {
-  mevami[index] =
-    meva.charAt(0).toUpperCase() + meva.slice(1).toLowerCase();
-});
-console.log(mevami);
-
-
-let region = ["Toshkent", "Samarqand", "Buxoro", "Xiva", "Namangan", "Andijon"];
-let regioni = region.slice(-3);
-console.log(regioni);
-
-
-
-let tillar = ["Ingliz tili", "Tarix", "Matematika", "Fizika", "Kimyo"];
-tillar.splice(2, 2, "Dasturlash");
-console.log(tillar);
+hech nima yoq
