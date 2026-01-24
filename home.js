bf8a6125d3c2055dff5817d45a1e9079122bb49d
Saidazim Shamshiyev <macbookpro13dyum@gmail.com>
2026-01-24 15:20:44 +0500

    Uyga vazifa 4ta ekan. bunisi

--- home.js
+++ home.js
@@ -1,10 +1,27 @@
-function juftmiToqmi(son) {
-    if (son % 2 === 0) {
-        return "Juft";
-    } else {
-        return "Toq";
-    }
-}
-console.log(juftmiToqmi(2));
-console.log(juftmiToqmi(1));
-console.log(juftmiToqmi(5));
+let prices = [150, 80, 200, 50, 120];
+let priCes = prices.filter(son => son < 100);
+console.log(priCes); 
+
+
+let scorec = [70, 85, 90, 95, 80];
+let qzude = scorec.filter(son => son > 90);
+console.log(qzude);
+  
+
+let mevami = ["olma", "banan", "apelsin", "shaftoli"];
+mevami.forEach((meva, index) => {
+  mevami[index] =
+    meva.charAt(0).toUpperCase() + meva.slice(1).toLowerCase();
+});
+console.log(mevami);
+
+
+let region = ["Toshkent", "Samarqand", "Buxoro", "Xiva", "Namangan", "Andijon"];
+let regioni = region.slice(-3);
+console.log(regioni);
+
+
+
+let tillar = ["Ingliz tili", "Tarix", "Matematika", "Fizika", "Kimyo"];
+tillar.splice(2, 2, "Dasturlash");
+console.log(tillar);
