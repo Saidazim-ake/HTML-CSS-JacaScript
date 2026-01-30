d4ff110aaf09ca9573693c95a2bbe300468bce98
mysterio <macbookpro13dyum@gmail.com>
2026-01-30 21:46:28 +0500

    hullas

---
+++ JSS.js
@@ -0,0 +1,20 @@
+//1//
+
+const mahsulot = "Laptop";
+const narxi = 8500000;
+const rangi = "Kumush";
+
+const xabar = `Siz ${rangi} rangli ${mahsulot} tanladingiz. Narxi: ${narxi} so'm`;
+
+console.log(xabar);
+
+//2//
+const mahsulot2 = "Telefon";
+const price = 5000000;
+const chegirma = 15; // foiz
+
+const noviynarx = price - (price * chegirma) / 100;
+
+const text = `${mahsulot2} - Asl narxi: ${price}, Chegirma: ${chegirma}%, Yangi narxi: ${noviynarx} so'm`;
+
+console.log(text);
